9945976688e617b947fa0cd1253499496c2949c9
Vincent Palmer <shift@someone.section.me>
2025-08-13 21:59:37 +0200

    feat: Add packages.default output to flake.nix

---
+++ flake.nix
@@ -0,0 +1,44 @@
+{ 
+  description = "A simple Rust project"; 
+ 
+  inputs = { 
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable"; 
+    flake-utils.url = "github:numtide/flake-utils"; 
+  }; 
+ 
+  outputs = { self, nixpkgs, flake-utils }: 
+    flake-utils.lib.eachDefaultSystem (system: 
+      let 
+        pkgs = nixpkgs.legacyPackages.${system}; 
+      in 
+      { 
+        devShells.default = pkgs.mkShell { 
+          packages = with pkgs; [ 
+            rustc 
+            cargo 
+            pkg-config 
+            openssl 
+          ]; 
+        }; 
+ 
+        packages.default = pkgs.rustPlatform.buildRustPackage { 
+          pname = "garnix-fetcher"; 
+          version = "0.1.0"; 
+          src = ./.; # Source is the current directory 
+ 
+          cargoLock = { 
+            lockFile = ./Cargo.lock; # Path to Cargo.lock 
+          }; 
+ 
+          # Build in release mode 
+          cargoBuildFlags = [ "--release" ]; 
+ 
+          # Install the binary 
+          installPhase = '' 
+            mkdir -p $out/bin 
+            cp target/release/garnix-fetcher $out/bin/ 
+          ''; 
+        }; 
+      } 
+    ); 
+}
